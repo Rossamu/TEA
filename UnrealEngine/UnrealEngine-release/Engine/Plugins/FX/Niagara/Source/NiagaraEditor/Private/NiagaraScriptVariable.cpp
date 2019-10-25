6588a68612d1a9ffc8676e344f5a0acd21e26ec0
Kutani Mio <mio041100505@gmail.com>
2019-10-26 00:52:33 +0900

    test update

--- UnrealEngine/UnrealEngine-release/Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/NiagaraScriptVariable.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "NiagaraScriptVariable.h"
-
-UNiagaraScriptVariable::UNiagaraScriptVariable(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-	
-}
--- path.bat
+++
@@ -1,2 +0,0 @@
-@set VS2017=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe
-@set UE4=%~dp0..\UnrealEngine
