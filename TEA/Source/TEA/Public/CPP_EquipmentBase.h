b7ef1ed2576df3561e98173ec1035bcfef49abd9
kyattyan <k.yattyan349245@gmail.com>
2020-10-03 04:32:39 +0900

    装備リストスクロール機能 加えて微修正

--- TEA/Source/TEA/Public/CPP_EquipmentBase.h
+++ TEA/Source/TEA/Public/CPP_EquipmentBase.h
@@ -14,6 +14,13 @@ class TEA_API UCPP_EquipmentBase : public UCPP_ItemBase
 {
 	GENERATED_BODY()
 public:
+
+	//It ecxplains how the item looks like, how useful it is, and so on.
+	//å©ÇΩñ⁄Ç‚ê´î\Ç…ä÷Ç∑ÇÈê‡ñæ
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+		FString Explanation;
+
+
 	UCPP_EquipmentBase();
 	~UCPP_EquipmentBase();
 };
